7990b7309fb680b2776252c27046593cab5d8096
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-11-08 19:01:51 +0530

    Create string-concatenate.cpp

---
+++ String-concatenate/string-concatenate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string s1,s2,s3;
+  cout<<"Enter the first string : ";
+  cin>>s1;
+  cout<<"Enter the second string : ";
+  cin>>s2;
+  s3+=s1;
+  s3+=" ";
+  s3+=s2;
+  cout<<"The string after concatenate is : "<<s3<<endl;
+  return 0;
+}
