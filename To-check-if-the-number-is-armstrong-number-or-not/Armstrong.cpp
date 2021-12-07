5f339e419ae07e85c673caaa371581a0e437d24d
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-12-07 20:31:38 +0530

    Create Armstrong.cpp

---
+++ To-check-if-the-number-is-armstrong-number-or-not/Armstrong.cpp
@@ -0,0 +1,27 @@
+/*
+  Program to check if the number is armstrong number or not
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num,temp,sum=0,s;
+  cout<<"Enter the number : ";
+  cin>>num;
+  temp=num;
+  while(temp!=0)
+  {
+    s=temp%10;
+    sum=sum+(s*s*s);
+    temp=temp/10;
+  }
+  if(sum==num)
+  {
+    cout<<num<<" is an armstrong number."<<endl;
+  }
+  else
+  {
+    cout<<num<<" is not an armstrong number."<<endl;
+  }
+  return 0;
+}
