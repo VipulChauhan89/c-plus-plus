216e474db08e0c81320bbb558a70f523084cab18
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-02-10 14:05:28 +0530

    Create pattern.cpp

---
+++ Pattern-Right-half-pyramid/pattern.cpp
@@ -0,0 +1,32 @@
+/*
+    Program to print the pattern of Right half pyramid
+            *
+          * *
+        * * *
+      * * * *
+    * * * * *
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter the value of n to print the pattern : ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i+j>=n-1)
+            {
+                cout<<" *";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
